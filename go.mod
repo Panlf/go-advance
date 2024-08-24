ab7ce1eb090d53c53850749d19b87fa438439e7f
panlf <liangfeng_pan@163.com>
2024-08-25 00:03:03 +0800

    空结构体应用场景

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module go-advance
 
-go 1.19
+go 1.23
 
-require github.com/loveleshsharma/gohive v0.0.0-20231119054340-f98e85308c6f // indirect
+require github.com/loveleshsharma/gohive v0.0.0-20231119054340-f98e85308c6f
---
+++ structs/empty_struct_test.go
@@ -0,0 +1,32 @@
+package structs
+
+import (
+	"fmt"
+	"testing"
+	"time"
+	"unsafe"
+)
+
+type EST struct {
+}
+
+func TestStructs(t *testing.T) {
+	var b EST
+	var c struct{}
+
+	fmt.Printf("b address %p size %d\n", &b, unsafe.Sizeof(b))
+	fmt.Printf("b address %p size %d\n", &c, unsafe.Sizeof(c))
+
+	students := make(map[string]struct{}, 10)
+	students["张三"] = EST{}
+	students["李四"] = struct{}{}
+	fmt.Println(len(students))
+
+	teachers := make(chan struct{}, 0)
+	go func() {
+		time.Sleep(3 * time.Second)
+		fmt.Println("子协程工作完毕")
+		teachers <- struct{}{}
+	}()
+	<-teachers
+}
